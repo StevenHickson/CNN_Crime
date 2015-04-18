f0145be8c25f4c30a1150d87b8da6299bdb3a80a
Steven Hickson <me@stevenhickson.com>
2015-04-18 14:18:09 -0400

    Finished arrest scraping method

--- get_arrests.py
+++ get_arrests.py
@@ -11,7 +11,7 @@ g = Grab()
 first_names=[]
 last_names=[]
 records=[]
-g.go(path + '/?page=1&results=10')
+g.go(path + '/?page=1&results=100000')
 for elem in g.doc.select('//ul/li/div/div[@class="title"]/a'):
 	#print elem
         #print '%s %s' % (elem.text(), elem.attr('href'))
@@ -28,7 +28,28 @@ for href in records:
 		mug = elem.attr('src')
 		img = mug.split('/')[-1]
 		#print '%s %s' % (mug, img)
-		subprocess.Popen(["wget", path + mug], cwd='data')
+		server=subprocess.Popen(["wget", '--quiet', path + mug], cwd='data')
 		imgs.append(img)
 
+output_file = open('arrests.txt','w')
 #We have the name and image, now lets get the conviction status
+for f,l,i, in itertools.izip(first_names,last_names,imgs):
+	lookup = 'http://webapps6.doc.state.nc.us/opi/offendersearch.do?method=list&searchLastName=' + l + '&searchFirstName=' + f
+	#print lookup
+	g.go(lookup)
+	if g.doc.text_search(u'Nothing found'):
+		status=0
+	else:
+		elem = g.doc.select('//tr/td[@class="tablelink"]/a')
+		#print '%s' % elem[0].attr('href')
+		g.go('http://webapps6.doc.state.nc.us/opi/' + elem[0].attr('href'))
+		if g.doc.text_search(u'INMATE'):
+			status=2
+		elif g.doc.text_search(u'PROBATION'):
+			status=1
+		else:
+			status=0
+	output_file.write("%s,%s,%s,%d\n" % (f,l,i,status))
+
+output_file.close()
+server.wait()
