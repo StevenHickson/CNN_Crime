299d63bbcf129af3cc6b5fa5a6901cb3c353de0a
Steven Hickson <me@stevenhickson.com>
2015-04-23 13:44:25 -0400

    :cocktail: Working now!

--- get_arrests.py
+++ get_arrests.py
@@ -15,51 +15,55 @@ while True:
 
 first_names=[]
 last_names=[]
+genders=[]
+races=[]
 dates=[]
 imgs=[]
 results=[]
 #&searchDOB=10/14/1988&
 g.go(sys.argv[1] + "?page=" + sys.argv[2])
 for elem in g.doc.select('//tr/td/a'):
-	print elem.attr('href')
 	results.append(elem.attr('href'))
 
 for a in results:
 	g.go(a)
-	i = 0
+	grab = 0
 	tmp = g.doc.select('//h1[@id="item-title"]/span[@itemprop="name"]')
 	fields = tmp[0].text().split(' ')
 	first_name = fields[0]
 	last_name = fields[-1]
-	for elem in g.doc.select('//div[@class="fieldvalues"]/div[@class="field"]/span[@class="value"]'):
-		if i == 1:
-			races = elem.text()
-		elif i == 2:
+	for elem in g.doc.select('//div[@class="fieldvalues"]/div[@class="field"]/span'):
+		if grab == 1:
+			race = elem.text()
+			grab = 0
+		elif grab == 2:
 			gender = elem.text()
-		elif i == 7:
+			grab = 0
+		elif grab == 3:
 			date = elem.text()
-		i = i + 1
-
+			grab = 0
+		if elem.text() == "Gender":
+			grab = 2
+		elif elem.text() == "Race":
+			grab = 1
+		elif elem.text().split(' ')[0] == "Birth":
+			grab = 3
 	dates.append(date)
 	last_names.append(last_name)
 	first_names.append(first_name)
-	print date
+	genders.append(gender)
+	races.append(race)
 	fields = date.split('/')
 	
-	img=""
-	i = 0
-	for elem in g.doc.select('//div[@class="full-image-container"]/div[@class="full-image"]/img[@class="hidden-wide"]'):
-		img_name = first_name + '_' + last_name + '_' + fields[0] + '_' + fields[1] + '_' + fields[2] + '.jpg'
-		if i > 0:
-			img+=','
-		img+=img_name
-		server=subprocess.Popen(["wget", '--quiet', '-O', img_name, elem.attr('src') ], cwd='data')
-		#print '%s %s %s %s' % (fields[1].split(' ')[1], fields[0], date.text(), img.attr('src'))
-	imgs.append(img)
+	elem = g.doc.select('//div[@class="full-image-container"]/div[@class="full-image"]/img[@class="hidden-wide"]')
+	img_name = first_name + '_' + last_name + '_' + fields[0] + '_' + fields[1] + '_' + fields[2] + '.jpg'
+	server=subprocess.Popen(["wget", '--quiet', '-O', img_name, elem[0].attr('src') ], cwd='data')
+	#print '%s %s %s %s' % (fields[1].split(' ')[1], fields[0], date.text(), img.attr('src'))
+	imgs.append(img_name)
 
 output_file = open(sys.argv[3],'w')
 #We have the name and image, now lets get the conviction status
-for f,l,d,i, in itertools.izip(first_names,last_names,dates,imgs):
+for f,l,d,s,r,i, in itertools.izip(first_names,last_names,dates,genders,races,imgs):
 	lookup = 'http://webapps6.doc.state.nc.us/opi/offendersearch.do?method=list&searchLastName=' + l + '&searchFirstName=' + f + '&searchDOB=' + d
 	#print lookup
 	print '%s %s' % (f, l)
@@ -76,6 +80,6 @@ for f,l,d,i, in itertools.izip(first_names,last_names,dates,imgs):
 			status=1
 		else:
 			status=0
-	output_file.write("%s,%s,%s,%d\n" % (f,l,i,status))
+	output_file.write("%s,%s,%s,%s,%s,%d\n" % (f,l,s,r,i,status))
 
 output_file.close()
